070399adc58a5408f85b5c6e45e9443ba3da05cc
ose-nyan5 <tsuramen0526@gmail.com>
2026-08-08 17:42:43 +0900

    update checkout urls

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -4,8 +4,8 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
 
 export async function POST() {
   const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
     mode: "payment",
+
     line_items: [
       {
         price_data: {
@@ -18,8 +18,8 @@ export async function POST() {
         quantity: 1,
       },
     ],
-    success_url: "http://localhost:3000/success",
-    cancel_url: "http://localhost:3000/cart",
+    success_url: "https://my-shop-gamma-sepia.vercel.app/success",
+    cancel_url: "https://my-shop-gamma-sepia.vercel.app/cart",
   })
 
   return Response.json({ url: session.url })
